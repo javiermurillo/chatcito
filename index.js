4ad401e750a33af3522d27a0a773c48160828620
Javier Murillo <jhmmurillo@gmail.com>
2013-08-23 04:08:03 -0500

    add index.js with app.set for render page.jade

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+
+app.set('views', __dirname + '/');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.use(express.static(__dirname + '/public'));
+app.listen(port);
+console.log("Listening on puertito " + port);
